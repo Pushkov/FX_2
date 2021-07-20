4acb411094a3084b435b63e4be180dd5f9c38603
Pushkov <nicsx@mail.ru>
2021-07-20 16:26:09 +0300

    For Executable Jar-file

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,7 @@
+import start.Main;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainWindow.fxml"));
         primaryStage.setTitle("Расчет нагрузок на ось. ver_0.2");
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 1000, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
